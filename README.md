e3dd4834c65ed880babe4d0eb44988598ff94590
thidapath <68995782+thidapath@users.noreply.github.com>
2023-09-13 00:55:16 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# liff-plugin-sizu25
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/liff-plugin-sizu25)
---
+++ index.js
@@ -0,0 +1,33 @@
+// Import stylesheets
+import './style.css';
+
+class BMIPlugin {
+  constructor() {
+  }
+  
+  install(context, option) {
+  }
+
+  localDate(lang, date) {
+  }
+
+  calculate(height, weight) {
+  }
+
+  greeting(lang, profile) {
+  }
+}
+
+// Binding HTML elements
+const h1Element = document.querySelector("h1");
+const h2Element = document.querySelector("h2");
+const h5Element = document.querySelector("h5");
+const heightElement = document.querySelector("#SYS");
+const weightElement = document.querySelector("#DIA");
+const btnElement = document.querySelector("button");
+
+// Activate LIFF Plugin
+
+// Display locale date
+
+// Add event listerner to Calculate button
